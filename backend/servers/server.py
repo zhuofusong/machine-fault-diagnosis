193506dc7e27518bc5090e381ca41b20f2ee2371
Zhuo Fu Song <bjsongzf@cn.ibm.com>
2020-04-14 06:56:22 -0700

    set up

--- backend/servers/server.py
+++ backend/servers/server.py
@@ -1,10 +1,17 @@
+import os
+import logging
 import pybrake
 
+# import all keys 
+with open(".env",'r') as f:
+    pybrake_key = f.readlines()[0]
+    pybrake_key = pybrake_key.split("=")[1]
+
 # pybrak notifier
-notifier = pybrake.Notifier(project_id=267681,project_key='',environment='production')
+notifier = pybrake.Notifier(project_id=267681,project_key=pybrake_key,environment='production')
+airbrake_handler = pybrake.LoggingHandler(notifier=notifier,level=logging.INFO)
 
+logger = logging.getLogger('test')
+logger.addHandler(airbrake_handler)
 
-try:
-    raise ValueError('hello')
-except Exception as err:
-    notifier.notify(err)
+logger.error('something bad happened')
