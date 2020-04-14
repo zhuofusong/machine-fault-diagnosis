b7ba8fb90593989492c1d222c8758c8801d0068c
Zhuo Fu Song <bjsongzf@cn.ibm.com>
2020-04-14 20:24:08 +0800

    set up

--- backend/servers/server.py
+++ backend/servers/server.py
@@ -1,7 +1,7 @@
 import pybrake
 
 # pybrak notifier
-notifier = pybrake.Notifier(project_id=267681,project_key='3f12af45d28eb6ac3d0891a463580efd',environment='production')
+notifier = pybrake.Notifier(project_id=267681,project_key='',environment='production')
 
 
 try:
